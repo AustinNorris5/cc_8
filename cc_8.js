c556882f0a975a3c880ef19bc009b72b9179bb4a
AustinNorris5 <ajnorris@usf.edu>
2025-02-16 14:10:33 -0500

    Task 2 - Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -10,3 +10,15 @@ function calculateSalary(baseSalary, bonus, taxRate) {
 //Log to the console and test data
 console.log(calculateSalary(5000, 500, 0.1));
 console.log(calculateSalary(7000, 1000, 0.15));
+
+//Task 2: Function Expression
+
+//Declare a function expression to determine the final price
+let calculateDiscount = function(price, discountRate) {
+    let finalPrice = price - (price * discountRate);
+    console.log(`Final Price: $${finalPrice.toFixed(2)}`);
+};
+
+//Log to the console and test data
+console.log(calculateDiscount(100, 0.2));
+console.log(calculateDiscount(250, 0.15));
